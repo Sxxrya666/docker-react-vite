bb50381ffb598adf419aec834c82d0b0a7b0c5a0
Sxxrya666 <qwqwqw.321@proton.me>
2024-12-30 16:17:06 +0000

    init commit bruv

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:alpine AS stageOne 
+
+WORKDIR /usr/src/app
+
+COPY package*.json .
+
+RUN npm i 
+
+COPY . .
+
+CMD [ "npm",  "run" ,"build" ]
+
+
+
+FROM nginx:1.27.3-alpine
+
+COPY --from=stageOne /build /usr/share/nginx/html
---
+++ error-learnings.js
@@ -0,0 +1,8 @@
+/* 
+
+https://stackoverflow.com/questions/71357957/unable-to-map-docker-port-from-vite-app-to-local
+https://stackoverflow.com/questions/78336694/error-cannot-find-module-rollup-rollup-linux-x64-musl-on-docker-container
+
+
+
+*/
